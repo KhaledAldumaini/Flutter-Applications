cb8e1c25692542ddd72e881c4e0652e10f670b18
Khaled_Aldumaini <khaledaldumaini258@gamil.com>
2025-12-11 09:03:11 +0300

    nav 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'first_screen.dart';
+
 void main() {
   runApp(ProfileApp());
 }
@@ -11,80 +13,39 @@ class ProfileApp extends StatefulWidget {
   _ProfileAppState createState() => _ProfileAppState();
 }
 
+
 class _ProfileAppState extends State<ProfileApp> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  String _email = '';
-  final TextEditingController _passwordController = TextEditingController();
-
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print('Name: $_name, Email: $_email, Password : ${_passwordController.text}');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Form Example')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _name = value!;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _email = value!;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    if (value.length < 6) {
-                      return 'Password must be at least 6 characters long';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Submit'),
-                ),
-              ],
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  TextEditingController Textcontroller=new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main Screen')),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              controller: Textcontroller,
             ),
-          ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen(data: Textcontroller.text)),
+                );
+              },
+              child: Text('Go to Second Screen'),
+            ),
+          ],
         ),
       ),
     );
